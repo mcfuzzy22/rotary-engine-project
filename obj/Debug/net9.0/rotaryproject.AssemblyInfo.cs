75e08beeae1d34a10692a0a28fb45a02085101a1
mcfuzzy22 <mcfuzzy22@gmail.com>
2025-05-25 16:15:41 -0400

    3d render works without button

--- obj/Debug/net9.0/rotaryproject.AssemblyInfo.cs
+++ obj/Debug/net9.0/rotaryproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rotaryproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a2a2c9876c2b4452299d1a517879d97540c9a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+612653d531ddd35d9bb6f839204bd1fa2cbdb07c")]
 [assembly: System.Reflection.AssemblyProductAttribute("rotaryproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rotaryproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
